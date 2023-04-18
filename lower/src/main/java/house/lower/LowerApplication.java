a9fde23bec9f5fd1f619254cb8c6d88c05e50610
kimillho <wowrladlfgh@gmail.com>
2023-04-18 14:35:52 +0900

    처음 프로젝트 커밋

---
+++ lower/src/main/java/house/lower/LowerApplication.java
@@ -0,0 +1,13 @@
+package house.lower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowerApplication.class, args);
+	}
+
+}
---
+++ lower/src/test/java/house/lower/LowerApplicationTests.java
@@ -0,0 +1,13 @@
+package house.lower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
